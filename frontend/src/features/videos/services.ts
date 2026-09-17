f7b03c91be7d58338dba5f64e9931d09b0002875
shiran86 <shiran.drori86@gmail.com>
2026-09-17 11:47:07 +0300

    add API_BASE_URL

--- frontend/src/features/videos/services.ts
+++ frontend/src/features/videos/services.ts
@@ -1,7 +1,8 @@
+import { API_BASE_URL } from '../../config'
 import type { Video } from './components/videoView/types'
 
 export const getVideo = async (id: number): Promise<Video> => {
-  const response = await fetch(`https://localhost:7105/api/v1/videos/${id}`, { credentials: 'include' })
+  const response = await fetch(`${API_BASE_URL}/videos/${id}`, { credentials: 'include' })
 
   if (!response.ok) {
     throw new Error(`Request failed with status ${response.status}`)
